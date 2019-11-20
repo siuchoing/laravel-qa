7e21e07c74b8aa0c5b0fc7661b1c0cccb1ee0c57
altantic <15200582@life.hlkbu.edu.hk>
2019-11-21 01:31:34 +0800

    Extend the form functionality to be able to update question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -63,7 +63,9 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        //
+        //$question = findOrFail($id);
+        return view('questions.edit', compact('question'));
+
     }
 
     /**
@@ -73,9 +75,10 @@ class QuestionsController extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+        $question->update($request->only('title', 'body'));
+        return redirect('/questions')->with('success', "Your question has beem updated.");
     }
 
     /**
