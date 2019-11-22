c8042fffeaac4395b1488dcc7d3321326e7d5ac8
altantic <15200582@life.hlkbu.edu.hk>
2019-11-22 23:32:48 +0800

    create fake data by factories and use boot() in Answer.php to increment answer_count

--- app/Answer.php
+++ app/Answer.php
@@ -20,4 +20,27 @@ class Answer extends Model
     {
         return \Parsedown::instance()->text($this->body);
     }
+
+    /*
+     * Eloquent Event:
+     *  - retrieved
+     *  - created, creating
+     *  - updated, updating
+     *  - saved, saving
+     *  - deleted, deleting
+     *  - restored, restoring
+    */
+    public static function boot() {
+        parent::boot();
+
+        # echo a message when create a answer each time
+        static::created(function($answer) {
+            $answer->question->increment('answers_count');          // to count answer
+            //$answer->question->save();
+            // echo "Answer created.\n";
+        });
+        static::saved(function($answer) {
+            // echo "Answer saved.\n";
+        });
+    }
 }
