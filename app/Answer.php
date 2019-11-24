3609106495342b6023dc10bd682e4ca029a35727
altantic <15200582@life.hlkbu.edu.hk>
2019-11-25 00:49:29 +0800

    Implement delete answer, and updated the answer_count while deleted by decrement

--- app/Answer.php
+++ app/Answer.php
@@ -35,15 +35,23 @@ class Answer extends Model
     public static function boot() {
         parent::boot();
 
-        # echo a message when create a answer each time
+        # echo a message when create a answer each time, and save new value
         static::created(function($answer) {
             $answer->question->increment('answers_count');          // to count answer
-            //$answer->question->save();
+            // $answer->question->save();                           // laravel will save automatically
             // echo "Answer created.\n";
         });
-        static::saved(function($answer) {
-            // echo "Answer saved.\n";
+
+        # update the answer_count while deleted an answer
+        static::deleted(function($answer) {
+            $answer->question->decrement('answers_count');          // to count answer
+            // $answer->question->save();                           // laravel will save automatically
+            // echo "Answer created.\n";
         });
+
+        // static::saved(function($answer) {
+        //     echo "Answer saved.\n";
+        // });
     }
 
     public function getCreatedDateAttribute()
