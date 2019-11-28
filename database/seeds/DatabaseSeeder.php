20ff91bdbcb2e4989f19c92d65c1391ab0516411
altantic <15200582@life.hlkbu.edu.hk>
2019-11-29 00:54:58 +0800

    Create Fake data for user's question in Many to many polymirphic relationship

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersQuestionAnswerTableSeeder::class,
             FavoritesTableSeeder::class,
+            VotablesTableSeeder::class
         ]);
     }
 };
