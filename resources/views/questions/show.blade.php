2d2500c1fa709b9b40c582eb8c4f68f87a5710cf
altantic <15200582@life.hlkbu.edu.hk>
2019-11-30 00:41:05 +0800

    Implement Dry Principle on Answer and Question in view

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -57,15 +57,14 @@
                             </div>
                             <div class="media-body">
                                 {!! $question->body_html !!}
-                                <div class="float-right">
-                                    <span class="text-muted">Answered {{ $question->created_date }}</span>
-                                    <div class="media mt-2">
-                                        <a href="{{ $question->user->url }}" class="pr-2">
-                                            <img style="width: 33px;" src="{{ $question->user->avatar }}">
-                                        </a>
-                                        <div class="media-body mt-1">
-                                            <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                        </div>
+                                <div class="row">
+                                    <div class="col-4"></div>
+                                    <div class="col-4"></div>
+                                    <div class="col-4">
+                                        @include('shared._author', [
+                                            'model' => $question,
+                                            'label' => 'Asked'
+                                        ])
                                     </div>
                                 </div>
                             </div>
