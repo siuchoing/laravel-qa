3510a40ad3d5d30aeb9efe8723e103141b11f087
altantic <15200582@life.hlkbu.edu.hk>
2019-11-30 18:15:03 +0800

    Set home url to question page

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 
 /*------------------- Question -----------------------*/
+Route::get('/', 'QuestionsController@index');
 Route::resource('questions', 'QuestionsController')->except('show');
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
 Route::post('/questions/{question}/favorites', 'FavoritesController@store')->name('questions.favorite');
