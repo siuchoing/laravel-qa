0f6c33646ddb6e9effe09bb4e6640e32772b3122
altantic <15200582@life.hlkbu.edu.hk>
2019-11-30 16:17:00 +0800

    Add class to allowed html code in purifier.php, and apply clean() to get rid of script code
    # After add class in purifier.php, where 'HTML.Allowed' => ..., p[style|class], ...
        >>> php artisan tinker
        >>> $str = "<p style='font-weight:bold' class='foo'>Hello</p><script>alert('Hello')</script>"
            => "<p style='font-weight:bold' class='foo'>Hello</p><script>alert('Hello')</script>"
        >>> clean($str)
            => "<p style="font-weight:bold;" class='foo'>Hello</p>"
    
        Method:
        1. To clean('textBody') before save DB              // save cleaned value to DB, and clean the html code
        2. To clean('textBody') before render to the page   // save original value to DB, and clean the html code [recommend this!!]

--- app/Question.php
+++ app/Question.php
@@ -50,7 +50,7 @@ class Question extends Model
 
     public function getBodyHtmlAttribute()
     {
-        return $this->bodyHtml();
+        return clean($this->bodyHtml());
     }
 
     public function answers()
