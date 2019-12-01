6e764b57474ec84c1c1193d2f9496ec905ca778a
altantic <15200582@life.hlkbu.edu.hk>
2019-12-01 23:48:04 +0800

    Rewrite the authorization policy into javascript code for Accept code

--- resources/js/components/Favorite.vue
+++ resources/js/components/Favorite.vue
@@ -30,10 +30,6 @@
             endpoint () {
                 return `/questions/${this.id}/favorites`;
             },
-
-            signedIn () {
-                return window.Auth.signedIn;
-            }
         },
         methods: {
             toggle () {
