fea6ca43ac678f8d8b56f7970e3d3d18f7ca7ae4
altantic <15200582@life.hlkbu.edu.hk>
2019-12-01 22:17:16 +0800

    Create vote button in vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,6 +27,7 @@ Vue.use(VueIziToast);
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+Vue.component('accept', require('./components/Accept.vue').default);
 Vue.component('answer', require('./components/Answer.vue').default);        // default for ES6
 Vue.component('user-info', require('./components/UserInfo.vue').default);
 Vue.component('favorite', require('./components/Favorite.vue').default);
---
+++ resources/js/components/Accept.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <a v-if="canAccept" title="Mark this answer as best answer"
+           :class="classes">
+            <i class="fas fa-check fa-2x"></i>
+        </a>
+
+        <a v-if="isBest" title="The question owner accept this answer as best answer"
+           :class="classes">
+            <i class="fas fa-check fa-2x"></i>
+        </a>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props: ['answer'],
+
+        data () {
+            return {        // return object
+                isBest: this.answer.is_best
+            }
+
+        },
+
+        computed: {
+            canAccept () {
+                return true;
+            },
+
+            accepted () {
+                return !this.canAccept && this.answer.is_best;
+            },
+
+            classes () {
+                return [
+                    'mt-2',
+                    this.accepted ? 'vote-accepted' : ''
+                ]
+            }
+        }
+    }
+</script>
