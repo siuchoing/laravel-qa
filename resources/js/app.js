731fc92516503e23d88d53c690dbdd5cb65cdbc0
altantic <15200582@life.hlkbu.edu.hk>
2019-12-04 07:24:04 +0800

    Fixed code

--- resources/js/app.js
+++ resources/js/app.js
@@ -31,8 +31,8 @@ Vue.use(Authorization);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 // only call inside component
-//Vue.component('accept', require('./components/Accept.vue').default);
-//Vue.component('favorite', require('./components/Favorite.vue').default);
+Vue.component('accept', require('./components/Accept.vue').default);
+Vue.component('favorite', require('./components/Favorite.vue').default);
 
 //Vue.component('answer', require('./components/Answer.vue').default);        // default for ES6
 Vue.component('user-info', require('./components/UserInfo.vue').default);
