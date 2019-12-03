1b7dd3bf08325190ede5edbe92d04cfddae86b77
altantic <15200582@life.hlkbu.edu.hk>
2019-12-03 21:27:47 +0800

    Implement Vote component for with success message

--- app/User.php
+++ app/User.php
@@ -92,14 +92,14 @@ class User extends Authenticatable
         // $u1->voteQuestions()->where('votable_id', $q1->id)->exists()     => true
         $voteQuestions = $this->voteQuestions();
 
-        $this->_vote($voteQuestions, $question, $vote);
+        return $this->_vote($voteQuestions, $question, $vote);
     }
 
     public function voteAnswer(Answer $answer, $vote)
     {
         $voteAnswers = $this->voteAnswers();
 
-        $this->_vote($voteAnswers, $answer, $vote);
+        return $this->_vote($voteAnswers, $answer, $vote);
     }
 
     private function _vote($relationship, $model, $vote)
@@ -116,6 +116,8 @@ class User extends Authenticatable
 
         $model->votes_count = $upVotes + $downVotes;
         $model->save();
+
+        return $model->votes_count;
     }
 
 }
--- resources/js/components/Vote.vue
+++ resources/js/components/Vote.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="d-fex flex-column vote-controls">
-        <a :title="title('up')"
+        <a @click.prevent="voteUp" :title="title('up')"
            class="vote-up" :class="classes">
             <i class="fas fa-caret-up fa-3x"></i>
         </a>
 
         <span class="votes-count">{{ count }}</span>
 
-        <a :title="title('down')"
+        <a @click.prevent="voteDown" :title="title('down')"
            class="vote-down" :class="classes">
             <i class="fas fa-caret-down fa-3x"></i>
         </a>
@@ -30,12 +30,16 @@
         computed: {
             classes() {
                 return this.signedIn ? '' : 'off';
-            }
+            },
+            endpoint(){
+              return `/${this.name}s/${this.id}/vote`;
+            },
         },
 
         data () {
             return {
-                count: this.model.votes_count
+                count: this.model.votes_count,
+                id: this.model.id
             }
         },
 
@@ -45,6 +49,35 @@
                     up: `This ${this.name} is useful`,
                     down: `This ${this.name} is not useful`,
                 }
+                return titles[voteType];
+            },
+
+            voteUp () {
+                this._vote(1);
+            },
+
+            voteDown () {
+                this._vote(-1);
+            },
+
+            _vote (vote) {
+                if (! this.signedIn) {
+                    this.$toast.warnging(`Please login to vote the ${this.name}`, "Warning", {
+                        timeout: 3000,
+                        position: 'bottomLeft',
+                    });
+
+                    return;
+                }
+                axios.post(this.endpoint, { vote })
+                    .then(res => {
+                        this.$toast.success(res.data.message, "Success", {
+                            timeout: 3000,
+                            position: 'bottomLeft'
+                        })
+
+                        this.count = res.data.votesCount;
+                    })
             }
         }
 
