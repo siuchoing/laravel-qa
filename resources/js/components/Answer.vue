9b4a5c9697266048078c8486f01d239d66b8cc36
altantic <15200582@life.hlkbu.edu.hk>
2019-12-07 14:15:13 +0800

    Utilizing vue.js mixins

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -31,15 +31,17 @@
 <script>
     import Vote from './Vote.vue';
     import UserInfo from './UserInfo.vue';
+    import modification from '../mixins/modification';
 
     export default {
         props: ['answer'],
+
+        mixins: [modification],
 
         components: { Vote, UserInfo },
 
         data() {
             return {
-                editing: false,
                 body: this.answer.body,
                 bodyHtml: this.answer.body_html,
                 id: this.answer.id,
@@ -49,77 +51,30 @@
         },
 
         methods: {
-
-            edit () {
-                // store value before edit
+p
+            setEditCache () {  // store value before edit
                 this.beforeEditCache = this.body;
-                this.editing = true;
             },
 
-            cancel () {
+            restoreFromCache () {
                 this.body = this.beforeEditCache;
-                this.editing = false;
             },
 
             // php artisan route:list --name="questions.answers.update"
             // you will get URL: questions/{question}/answers/{answer}
-            update() {
-                axios.patch(this.endpoint, {
+            payload () {
+                return {
                     body: this.body
-                })
-                .then(res => {      // return reponseObject
-                    this.editing = false;
-                    this.bodyHtml = res.data.body_html;
-                    //alert('sad');
-                    this.$toast.success(res.data.message, "Success", { timeout: 3000 });
-                })
-                .catch(err => {
-                    // console.log(error.response.data.errors.body[0]);
-                    this.$toast.error(err.response.data.message, "Error", { timeout: 3000 });
-                    //alert(err.response.data.message);
-                });
+                };
             },
 
-            destroy () {
-                // Move code for Question Methods from "https://izitoast.marcelodolza.com/#Start"
-                this.$toast.question('Are you sure about that?', "Confirm", {
-                    timeout: 20000,
-                    close: false,
-                    overlay: true,
-                    displayMode: 'once',
-                    id: 'question',
-                    zindex: 999,
-                    title: 'Hey',
-                    position: 'center',
-                    buttons: [
-                        ['<button><b>YES</b></button>', (instance, toast) => {
-
-                            axios.delete(this.endpoint)
-                                .then(res => {
-                                    // fadeOut($running_time, $action_after_running)
-                                    // $(this.$el).fadeOut(500, () =>{
-                                    //     this.$toast.error(res.data.message, "Success", { timeout: 3000 });
-                                    // })
-                                    this.$emit('deleted')
-                                });
-
-                            instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-
-                        }, true],
-                        ['<button>NO</button>', function (instance, toast) {
-
-                            instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-
-                        }],
-                    ],
-                    onClosing: function(instance, toast, closedBy){
-                        console.info('Closing | closedBy: ' + closedBy);
-                    },
-                    onClosed: function(instance, toast, closedBy){
-                        console.info('Closed | closedBy: ' + closedBy);
-                    }
-                });
-            },
+            delete () {
+                axios.delete(this.endpoint)
+                    .then(res => {
+                        this.$toast.success(res.data.message, "Success", { timeout: 2000 });
+                        this.$emit('deleted')
+                    });
+            }
         },
 
         computed: {
@@ -129,7 +84,7 @@
 
             endpoint () {
                 return `/questions/${this.questionId}/answers/${this.id}`;
-            },
+            }
         }
     }
 
--- resources/js/components/Question.vue
+++ resources/js/components/Question.vue
@@ -56,9 +56,12 @@
 <script>
     import Vote from './Vote.vue';
     import UserInfo from './UserInfo.vue';
+    import modification from '../mixins/modification';
 
     export default {
         props: ['question'],
+
+        mixins: [modification],
 
         components: { Vote, UserInfo },
 
@@ -67,11 +70,42 @@
                 title: this.question.title,
                 body: this.question.body,
                 bodyHtml: this.question.body_html,
-                editing: false,
                 id: this.question.id,
                 beforeEditCache: {}
             }
         },
+
+        methods: {
+            setEditCache () {
+                this.beforeEditCache = {
+                    body: this.body,
+                    title: this.title
+                };
+            },
+
+            restoreFromCache () {
+                this.body = this.beforeEditCache.body;
+                this.title = this.beforeEditCache.title;
+            },
+
+            payload () {
+                return {
+                    body: this.body,
+                    title: this.title
+                };
+            },
+
+            delete () {
+                axios.delete(this.endpoint)
+                    .then(({data}) => {
+                        this.$toast.success(data.message, "Success", {timeout: 2000});
+                    });
+                setTimeout(() => {
+                    window.location.href = "/questions";
+                }, 3000);
+            }
+        },
+
         computed: {
             isInvalid () {
                 return this.body.length < 10 || this.title.length < 10;
@@ -80,61 +114,5 @@
                 return `/questions/${this.id}`;
             }
         },
-        methods: {
-            edit () {
-                this.beforeEditCache = {
-                    body: this.body,
-                    title: this.title
-                };
-                this.editing = true;
-            },
-            cancel () {
-                this.body = this.beforeEditCache.body;
-                this.title = this.beforeEditCache.title;
-                this.editing = false;
-            },
-            update () {
-                axios.put(this.endpoint, {
-                    body: this.body,
-                    title: this.title
-                })
-                    .catch(({response}) => {
-                        this.$toast.error(response.data.message, "Error", { timeout: 3000 });
-                    })
-                    .then(({data}) => {
-                        this.bodyHtml = data.body_html;
-                        this.$toast.success(data.message, "Success", { timeout: 3000 });
-                        this.editing = false;
-                    })
-            },
-            destroy () {
-                this.$toast.question('Are you sure about that?', "Confirm", {
-                    timeout: 20000,
-                    close: false,
-                    overlay: true,
-                    displayMode: 'once',
-                    id: 'question',
-                    zindex: 999,
-                    title: 'Hey',
-                    position: 'center',
-                    buttons: [
-                        ['<button><b>YES</b></button>', (instance, toast) => {
-
-                            axios.delete(this.endpoint)
-                                .then(({data}) => {
-                                    this.$toast.success(data.message, "Success", { timeout: 2000 });
-                                });
-                            setTimeout(() => {
-                                window.location.href = "/questions";
-                            }, 3000);
-                            instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-                        }, true],
-                        ['<button>NO</button>', function (instance, toast) {
-                            instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-                        }],
-                    ]
-                });
-            }
-        }
     }
 </script>
