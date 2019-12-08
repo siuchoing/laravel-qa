0117e8fec6f23e99f158a445062f9863ad7c758f
altantic <15200582@life.hlkbu.edu.hk>
2019-12-08 14:36:06 +0800

    push the latest app.js for error fix

--- public/js/app.js
+++ public/js/app.js
@@ -2501,8 +2501,8 @@ __webpack_require__.r(__webpack_exports__);
 // method 1: To change in prism.css, and then recompile the code
 // we are using method 2: copy all these theme to a public directory, and reference the theme in layout.
 
-var md = new markdown_it__WEBPACK_IMPORTED_MODULE_0___default.a();
-md.use(markdown_it_prism__WEBPACK_IMPORTED_MODULE_1___default.a);
+var md = new markdown_it__WEBPACK_IMPORTED_MODULE_0__["MarkdownIt"]();
+md.use(markdown_it_prism__WEBPACK_IMPORTED_MODULE_1__["prism"]);
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: ['body', 'name'],
   // methods: {
@@ -2525,7 +2525,7 @@ md.use(markdown_it_prism__WEBPACK_IMPORTED_MODULE_1___default.a);
   mounted: function mounted() {
     // remove scroll bar in write tab for edit, and replace this line with auto size and pass the text area in from a NodeList
     //autosize(document.querySelectorAll('textarea'));
-    autosize__WEBPACK_IMPORTED_MODULE_2___default()(this.$el.querySelectorAll('textarea')); //console.log('mounted hook');
+    Object(autosize__WEBPACK_IMPORTED_MODULE_2__["autosize"])(this.$el.querySelectorAll('textarea')); //console.log('mounted hook');
   },
   // This updated life cycle hook executed after data changes in our component and a DOM re-render
   updated: function updated() {//console.log('updated hook');
@@ -37325,8 +37325,12 @@ function loadPrismLang(lang) {
   let langObject = _prismjs.default.languages[lang];
 
   if (langObject === undefined) {
-    (0, _components.default)([lang]);
-    langObject = _prismjs.default.languages[lang];
+      try {
+          __webpack_require__(489)("./prism-" + lang);
+
+          return _prismjs.default.languages[lang];
+      } catch (e) {// nothing to do
+      }
   }
 
   return langObject;
