f3f0c0e6eb53b612bde760f8830b06c3cad85f41
altantic <15200582@life.hlkbu.edu.hk>
2019-12-10 01:20:13 +0800

    Integrating Markdown Editor in Answer Component

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -2,15 +2,17 @@
     <div class="media post" >
         <vote :model="answer" name="answer"></vote>
         <div class="media-body">
-            <form v-if="editing" @submit.prevent="update">
+            <form v-show="authorize('modify', answer) && editing" @submit.prevent="update">
                 <div class="form-group">
-                    <textarea rows="10" v-model="body" class="form-control" required></textarea>
+                    <m-editor :body="body" :name="uniqueName">
+                        <textarea rows="10" v-model="body" class="form-control" required></textarea>
+                    </m-editor>
                 </div>
                 <button class="btn btn-primary" type="submit" :disabled="isInvalid">Update</button>
                 <button class="btn btn-outline-secondary" type="button" @click.prevent="cancel">Cancel</button>
             </form>
-            <div v-else>
-                <div v-html="bodyHtml"></div>
+            <div v-show="!editing">
+                <div v-html="bodyHtml" ref="bodyHtml"></div>
                 <div class="row">
                     <div class="col-4">
                         <div class="ml-auto">
@@ -83,6 +85,10 @@
 
             endpoint () {
                 return `/questions/${this.questionId}/answers/${this.id}`;
+            },
+
+            uniqueName () {
+                return `answer-${this.id}`;
             }
         }
     }
--- resources/js/components/Question.vue
+++ resources/js/components/Question.vue
@@ -12,7 +12,7 @@
                     <div class="media">
                         <div class="media-body">
                             <div class="form-group">
-                                <m-editor :body="body">
+                                <m-editor :body="body" :name="uniqueName">
                                     <textarea rows="10" v-model="body" class="form-control" required></textarea>
                                 </m-editor>
                             </div>
@@ -115,6 +115,10 @@
             },
             endpoint () {
                 return `/questions/${this.id}`;
+            },
+
+            uniqueName () {
+                return `question-${this.id}`;
             }
         },
     }
