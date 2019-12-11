c6c2ce542a4908e57eacc6670dbfa7ca34e1090b
altantic <15200582@life.hlkbu.edu.hk>
2019-12-11 23:01:02 +0800

    Installation of  "Get Postman"
    Download APP "Get Postman"
    https://www.getpostman.com/downloads/
    
    Watch img for tutorial of Get PostMan:
    1. https://ibb.co/Y7TXfZM
    2. https://ibb.co/DDZ14Vc
    3. https://ibb.co/FYsrGF4
    4. https://ibb.co/Wv9NByk
    5. https://ibb.co/Q8SBHdm

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,29 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    //////////////////
+    //Client ID: 1
+    //Client secret: jsbYfXl0Bwri0DQtMAxPoluS32rpiRh62yrLbDF8
+    //Password grant client created successfully.
+    //Client ID: 2
+    //Client secret: ucH20VlC2OzW2SdAFEilrCLXO83IBPK1Fho8eWId
+    ////////////////////////////////////////////////////////////
+    public function getToken(Request $request)
+    {
+        $request->request->add([
+            'grant_type' => 'password',
+            'client_id' => 2,
+            'client_secret' => 'ucH20VlC2OzW2SdAFEilrCLXO83IBPK1Fho8eWId',
+            'username' => $request->username,
+            'password' => $request->password,
+        ]);
+
+        // make sure APP_URL = http://localhost:8000 in .env
+        $requestToken = Request::create(env('APP_URL') . '/oauth/token', 'post');
+        $response = Route::dispatch($requestToken);
+
+        return $response;
     }
 }
