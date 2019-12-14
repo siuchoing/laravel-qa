5a8c48b497444563f6890e1fdacd22592640bc16
altantic <15200582@life.hlkbu.edu.hk>
2019-12-14 12:58:09 +0800

    Fix Creating Vote Question Endpoints

--- app/Http/Controllers/Api/VoteQuestionController.php
+++ app/Http/Controllers/Api/VoteQuestionController.php
@@ -8,24 +8,13 @@ use Illuminate\Http\Request;
 
 class VoteQuestionController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function __invoke(Question $question)
     {
         $vote = (int) request()->vote;
-
         $votesCount = auth()->user()->voteQuestion($question, $vote);
-
-        if (request()->expectsJson()) {
-            return response()->json([
-                'message' => 'Thanks for the feedback',
-                'votesCount' => $votesCount
-            ]);
-        }
-
-        return back();
+        return response()->json([
+            'message' => 'Thanks for the feedback',
+            'votesCount' => $votesCount
+        ]);
     }
 }
