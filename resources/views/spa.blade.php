211c2925a84313af30a5d2e05c041d5c19850432
altantic <15200582@life.hlkbu.edu.hk>
2019-12-16 00:06:21 +0800

    Fix fail to connect hyberlink and display css style

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -21,7 +21,8 @@
 <div id="app">
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
-            <route-link class="nabar-brand" :to="{ name: 'home' }">Laravel Q/A</route-link>
+            <router-link class="navbar-brand" :to="{ name: 'home' }">Laravel Q/A</router-link>
+            
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
             </button>
