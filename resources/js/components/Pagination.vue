fe7733ad1778a8f3977b254c77ae1fb9d4cebf1b
altantic <15200582@life.hlkbu.edu.hk>
2019-12-17 02:46:19 +0800

    Setting up the button's state
    1. use the disabled attribute bindings to isFirst and isLast to respective buttons
    2. set function for passing current page while changing its value

--- resources/js/components/Pagination.vue
+++ resources/js/components/Pagination.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row align-items-center">
         <div class="col">
-            <button class="btn btn-outline-secondary">Newer</button>
+            <button :disabled="isFirst" @click="prev" class="btn btn-outline-secondary">Newer</button>
         </div>
         <!-- 1st column -->
 
@@ -9,7 +9,7 @@
         <!-- 2nd column -->
 
         <div class="col text-right">
-            <button class="btn btn-outline-secondary">Older</button>
+            <button :disabled="isLast" ...>Older</button>
         </div>
         <!-- 3rd column -->
     </div>
@@ -22,6 +22,14 @@
         computed: {
             pagesInfo () {
                 return `Page ${this.meta.current_page} of ${this.meta.last_page}`
+            },
+
+            isFirst () {
+                return this.meta.current_page === 1;
+            },
+
+            isLast () {
+                return this.meta.current_page === this.meta.last_page;
             }
         }
     }
