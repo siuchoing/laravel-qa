3a83526d929418805b204a019e2cc945a7ccb631
altantic <15200582@life.hlkbu.edu.hk>
2019-12-17 01:11:31 +0800

    Create Questions.vue and copy .card-body content from QuestionsPage.vue into this vue component

---
+++ resources/js/components/Questions.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <div class="card-body">
+            <div v-if="questions.length">
+                <question-excerpt v-for="question in questions" :question="question" :key="question.id"></question-excerpt>
+            </div>
+            <div v-else class="alert alert-warning">
+                <strong>Sorry</strong> There are no questions available.
+            </div>
+
+            <!-- pagination goes here -->
+        </div>
+    </div>
+</template>
