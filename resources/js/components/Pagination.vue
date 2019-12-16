d04b2ca0ef2e9fe4693e10c85e209cac0f993e91
altantic <15200582@life.hlkbu.edu.hk>
2019-12-17 01:29:39 +0800

    add 3 columns by making use the bootstrap col class

--- resources/js/components/Pagination.vue
+++ resources/js/components/Pagination.vue
@@ -1,5 +1,10 @@
-<template>
-    <div class="row align-items-center">
+<div class="row align-items-center">
+    <div class="col"></div>
+    <!-- 1st column -->
 
-    </div>
-</template>
+    <div class="col"></div>
+    <!-- 2nd column -->
+
+    <div class="col"></div>
+    <!-- 3rd column -->
+</div>
