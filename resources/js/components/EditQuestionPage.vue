dc7805972888bb743ea8af6fc57bd795bde1c2ab
altantic <15200582@life.hlkbu.edu.hk>
2019-12-19 07:14:14 +0800

    1. Create EditQuestionPage.vue and modify it
    In this method we hit the Edit question endpoint using get request. And assign the title and body from api response to title and body property respectively.

---
+++ resources/js/components/EditQuestionPage.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex align-items-center">
+                            <h2>Edit Question</h2>
+                            <div class="ml-auto">...</div>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        <question-form
+                            @submitted="update"
+                            :is-edit="true"></question-form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuestionForm from '../components/QuestionForm.vue'
+    import EventBus from '../event-bus'
+    export default {
+        components: { QuestionForm },
+
+        methods: {
+            update (data) {
+                // this.$route.params.id will capture the id parameter from the URL
+                axios.put('/questions/' + this.$route.params.id, data)
+                    .then(({ data }) => {
+                        this.$router.push({ name: 'questions' })
+                        this.$toast.success(data.message, "Success")
+                    })
+                    .catch(({ response }) => {
+                        EventBus.$emit('error', response.data.errors)
+                    })
+            }
+        }
+    }
+</script>
