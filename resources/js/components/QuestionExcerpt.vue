cd0308b30f6dd118f1d5b8172e3ed7625c53781a
altantic <15200582@life.hlkbu.edu.hk>
2019-12-19 22:27:49 +0800

    Attaching the click event on Delete button without using SPA, and using axios library to hit the Delete Question endpoint

--- resources/js/components/QuestionExcerpt.vue
+++ resources/js/components/QuestionExcerpt.vue
@@ -19,9 +19,12 @@
                         :to="{ name: 'questions.edit', params: { id: question.id } }"
                         v-if="authorize('modify', question)"
                         class="btn btn-sm btn-outline-info">Edit</router-link>
-                    <form v-if="authorize('deleteQuestion', question)" class="form-delete" method="post" action="#">
-                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                    </form>
+
+                    <!-- Attaching the click event on Delete button -->
+                    <button
+                        v-if="authorize('deleteQuestion', question)"
+                        class="btn btn-sm btn-outline-danger"
+                        @click="destroy">Delete</button>
                 </div>
             </div>
             <p class="lead">
