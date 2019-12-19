ed1bddfdc7ec66afd6f5d62c37ce6b2a9e4d4999
altantic <15200582@life.hlkbu.edu.hk>
2019-12-19 23:09:44 +0800

    define the remove method in methods property, and increment the count variable

--- resources/js/components/Questions.vue
+++ resources/js/components/Questions.vue
@@ -2,7 +2,11 @@
     <div>
         <div class="card-body">
             <div v-if="questions.length">
-                <question-excerpt v-for="question in questions" :question="question" :key="question.id"></question-excerpt>
+                <question-excerpt
+                    @deleted="remove(index)"
+                    v-for="(question, index) in questions"
+                    :question="question"
+                    :key="question.id"></question-excerpt>
             </div>
             <div v-else class="alert alert-warning">
                 <strong>Sorry</strong> There are no questions available.
@@ -46,6 +50,11 @@
                         this.links = data.links;
                         this.meta = data.meta;
                     })
+            },
+
+            remove (index) {
+                this.questions.splice(index, 1)
+                this.count--
             }
         },
 
